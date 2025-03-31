8f9b87e1f56b714f87695019a6d449de60611ab2
Oleksandr Zhyzhchenko <zhyzhchenko.ag@gmail.com>
2025-03-31 19:50:06 +0300

    handle case with multiple appends to the document

--- listner-extensions.js
+++ listner-extensions.js
@@ -1,19 +1,29 @@
 (function () {
     'use strict';
 
-    Object.defineProperty(window.Lampa.Card.prototype, 'build', {
-        get: function () {
-            return this._build;
-        },
-        set: function (value) {
-            this._build = function() {
-                value.apply(this);
-    
-                Lampa.Listener.send('card', {
-                    type: 'build',
-                    object: this
-                });
-            }.bind(this);
+    function start() {
+        if (window.lampa_listner_extensions) {
+            return;
         }
-    });
+
+        window.lampa_listner_extensions = true;
+
+        Object.defineProperty(window.Lampa.Card.prototype, 'build', {
+            get: function () {
+                return this._build;
+            },
+            set: function (value) {
+                this._build = function () {
+                    value.apply(this);
+
+                    Lampa.Listener.send('card', {
+                        type: 'build',
+                        object: this
+                    });
+                }.bind(this);
+            }
+        });
+    }
+
+    start();
 })()
