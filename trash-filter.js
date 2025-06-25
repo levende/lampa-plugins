6726cd740a6b07caa5584c255df4ae425f837761
levende <zhyzhchenko.ag@gmail.com>
2025-06-25 16:15:30 +0300

    Update trash-filter.js

--- trash-filter.js
+++ trash-filter.js
@@ -9,7 +9,7 @@
         window.trash_filter_plugin = true;
 
        Lampa.Listener.follow('request_secuses', function (event) {
-            if (event.params.url.indexOf(Lampa.TMDB.api('')) != -1 && event.params.url.indexOf('search') != -1 && event.data && Array.isArray(event.data.results)) {
+            if (event.params.url.indexOf(Lampa.TMDB.api('')) != -1 && event.params.url.indexOf('search') == -1 && event.data && Array.isArray(event.data.results)) {
                 event.data.results = event.data.results.filter(function(item) {
                     return item.vote_average !== 0 && (item.vote_count === undefined || item.vote_count > 50);
                 });
