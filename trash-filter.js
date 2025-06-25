0c4e2a16e23b6c90a85170888802b4e5ed80bde8
levende <zhyzhchenko.ag@gmail.com>
2025-06-25 17:26:17 +0300

    Update trash-filter.js

--- trash-filter.js
+++ trash-filter.js
@@ -1,6 +1,44 @@
 (function () {
     'use strict';
 
+    var PreFilters = {
+        applyMinVotes: function(baseUrl) {
+            baseUrl += '&vote_count.gte=' + 1;
+            return baseUrl;
+        },
+        applyWithoutKeywords: function(baseUrl) {
+            var baseExcludedKeywords = [
+                '346488',
+                '158718',
+                '41278'
+            ];
+
+            baseUrl += '&without_keywords=' + encodeURIComponent(baseExcludedKeywords.join(','));
+            return baseUrl;
+        }
+    }
+
+    var PostFilters = {
+        applyMinVotes(results) {
+            return results.filter(function(item) {
+                if (!item.original_language) {
+                    return true;
+                }
+
+                var lang = item.original_language.toLowerCase();
+                if (lang == 'uk' || lang == 'ru') {
+                    return true;
+                }
+
+                return item.vote_count > 50;
+            });
+        }
+    }
+
+    function isFilterApplicable(baseUrl) {
+        return baseUrl.indexOf(Lampa.TMDB.api('')) > -1 && baseUrl.indexOf('search') == -1;
+    }
+
     function start() {
         if (window.trash_filter_plugin) {
             return;
@@ -8,11 +46,16 @@
 
         window.trash_filter_plugin = true;
 
-       Lampa.Listener.follow('request_secuses', function (event) {
-            if (event.params.url.indexOf(Lampa.TMDB.api('')) != -1 && event.params.url.indexOf('search') == -1 && event.data && Array.isArray(event.data.results)) {
-                event.data.results = event.data.results.filter(function(item) {
-                    return item.vote_average !== 0 && (item.vote_count === undefined || item.vote_count > 50);
-                });
+        Lampa.Listener.follow('request_before', function(event) {
+            if (isFilterApplicable(event.params.url)) {
+                event.params.url = PreFilters.applyMinVotes(event.params.url);
+                event.params.url = PreFilters.applyWithoutKeywords(event.params.url);
+            }
+        });
+
+        Lampa.Listener.follow('request_secuses', function (event) {
+            if (isFilterApplicable(event.params.url) && event.data && Array.isArray(event.data.results)) {
+                event.data.results = PostFilters.applyMinVotes(event.data.results);
             }
         });
     }
