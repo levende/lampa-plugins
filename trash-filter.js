16fc34c5162d54355e42012eb852d4e2d72a3dcc
levende <zhyzhchenko.ag@gmail.com>
2025-06-25 19:24:18 +0300

    trash-filter: refactoring

--- trash-filter.js
+++ trash-filter.js
@@ -1,39 +1,61 @@
 (function () {
     'use strict';
 
-    var PreFilters = {
-        applyMinVotes: function(baseUrl) {
-            baseUrl += '&vote_count.gte=' + 1;
-            return baseUrl;
-        },
-        applyWithoutKeywords: function(baseUrl) {
-            var baseExcludedKeywords = [
-                '346488',
-                '158718',
-                '41278'
-            ];
+    var preFilters = {
+        filters: [
+            function(baseUrl) {
+                baseUrl += '&vote_count.gte=' + 1;
+                return baseUrl;
+            },
+            function(baseUrl) {
+                var baseExcludedKeywords = [
+                    '346488',
+                    '158718',
+                    '41278'
+                ];
 
-            baseUrl += '&without_keywords=' + encodeURIComponent(baseExcludedKeywords.join(','));
-            return baseUrl;
+                baseUrl += '&without_keywords=' + encodeURIComponent(baseExcludedKeywords.join(','));
+                return baseUrl;
+            }
+        ],
+        apply: function(baseUrl) {
+            var resultUrl = baseUrl;
+
+            for (var i = 0; i < this.filters.length; i++) {
+                resultUrl = this.filters[i](resultUrl);
+            }
+
+            return resultUrl;
         }
-    }
+    };
 
-    var PostFilters = {
-        applyMinVotes(results) {
-            return results.filter(function(item) {
-                if (!item.original_language) {
-                    return true;
-                }
+    var postFilters = {
+        filters: [
+            function(results) {
+                return results.filter(function(item) {
+                    if (!item.original_language) {
+                        return true;
+                    }
 
-                var lang = item.original_language.toLowerCase();
-                if (lang == 'uk' || lang == 'ru') {
-                    return true;
-                }
+                    var lang = item.original_language.toLowerCase();
+                    if (lang == 'uk' || lang == 'ru') {
+                        return true;
+                    }
 
-                return item.vote_count > 50;
-            });
+                    return item.vote_count > 50;
+                });
+            }
+        ],
+        apply: function(results) {
+            var clone = Lampa.Arrays.clone(results);
+
+            for (var i = 0; i < this.filters.length; i++) {
+                clone = this.filters[i](results);
+            }
+
+            return clone;
         }
-    }
+    };
 
     function isFilterApplicable(baseUrl) {
         return baseUrl.indexOf(Lampa.TMDB.api('')) > -1 && baseUrl.indexOf('search') == -1;
@@ -48,14 +70,13 @@
 
         Lampa.Listener.follow('request_before', function(event) {
             if (isFilterApplicable(event.params.url)) {
-                event.params.url = PreFilters.applyMinVotes(event.params.url);
-                event.params.url = PreFilters.applyWithoutKeywords(event.params.url);
+                event.params.url = preFilters.apply(event.params.url);
             }
         });
 
         Lampa.Listener.follow('request_secuses', function (event) {
             if (isFilterApplicable(event.params.url) && event.data && Array.isArray(event.data.results)) {
-                event.data.results = PostFilters.applyMinVotes(event.data.results);
+                event.data.results = postFilters.apply(event.data.results);
             }
         });
     }
