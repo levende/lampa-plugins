48d32345f371f419bc244c1e778a2b80e0e10c01
levende <zhyzhchenko.ag@gmail.com>
2025-06-30 11:21:13 +0300

    history-filter: small more button and thrown cards

--- history-filter.js
+++ history-filter.js
@@ -25,6 +25,11 @@
 
                     var favoriteItem = Lampa.Favorite.check(item);
                     var watched = !!favoriteItem && !!favoriteItem.history;
+                    var thrown = !!favoriteItem && favoriteItem.thrown;
+
+                    if (thrown) {
+                        return false;
+                    }
 
                     if (!watched) {
                         return true;
@@ -162,6 +167,15 @@
             && baseUrl.indexOf('/person/') === -1;
     }
 
+    function hasMorePage(data) {
+        return !!data
+            && Array.isArray(data.results)
+            && data.original_length !== data.filter_length
+            && data.page === 1
+            && !!data.total_pages
+            && data.total_pages > 1;
+    }
+
     function start() {
         if (window.history_filter_plugin) {
             return;
@@ -170,15 +184,37 @@
         window.history_filter_plugin = true;
 
         Lampa.Listener.follow('line', function (event) {
-            if (event.type !== 'append'
-                || !event.data
-                || !Array.isArray(event.data.results)
-                || !event.data.original_length
-                || !event.data.filter_length) {
+            if (event.type !== 'visible' || !hasMorePage(event.data)) {
                 return;
             }
 
-            if (event.data.filter_length === event.data.original_length) {
+            var lineHeader$ = $(event.body.closest('.items-line')).find('.items-line__head');
+            var hasMoreBtn = lineHeader$.find('.items-line__more').length !== 0;
+
+            if (hasMoreBtn) return;
+
+            var button = document.createElement('div');
+            button.classList.add('items-line__more');
+            button.classList.add('selector');
+            button.innerText = Lampa.Lang.translate('more');
+
+            button.addEventListener('hover:enter', function() {
+                Lampa.Activity.push({
+                    url: event.data.url,
+                    title: event.data.title || Lampa.Lang.translate('title_category'),
+                    component: 'category_full',
+                    page: 1,
+                    genres: event.params.genres,
+                    filter: event.data.filter,
+                    source: event.data.source || event.params.object.source
+                });
+            });
+
+            lineHeader$.append(button);
+        });
+
+        Lampa.Listener.follow('line', function (event) {
+            if (event.type !== 'append' || !hasMorePage(event.data)) {
                 return;
             }
 
@@ -189,10 +225,9 @@
 
         Lampa.Listener.follow('request_secuses', function (event) {
             if (isFilterApplicable(event.params.url) && event.data && Array.isArray(event.data.results)) {
-                event.data.original_length = event.data.results.length;
+                if (!event.data.original_length) event.data.original_length = event.data.results.length;
                 event.data.results = postFilters.apply(event.data.results);
                 event.data.filter_length = event.data.results.length;
-                // event.data.results.length = event.data.original_length;
             }
         });
     }
